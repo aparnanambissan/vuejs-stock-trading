fd73f1c4635891a2d8592d097a944865f176dfdf
aparnanambissan <33481201+aparnanambissan@users.noreply.github.com>
2020-06-02 17:57:57 -0400

    Initial Commit

---
+++ src/components/Header.vue
@@ -0,0 +1,40 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <router-link class="navbar-brand" to="/">Stock Trader</router-link>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <router-link class="navbar-brand" to="/portfolio">
+            <a>Portfolio</a>
+          </router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="navbar-brand" to="/stocks">
+            <a>Stocks</a>
+          </router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="navbar-brand" to="/">
+            <a>End Day</a>
+          </router-link>
+        </li>
+        <li class="nav-item dropdown">
+          <a
+            class="nav-link dropdown-toggle"
+            href="#"
+            id="navbarDropdownMenuLink"
+            role="button"
+            data-toggle="dropdown"
+            aria-haspopup="true"
+            aria-expanded="false"
+          >Save & Load</a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+            <a class="dropdown-item" href="#">Save Data</a>
+            <a class="dropdown-item" href="#">Load Data</a>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
